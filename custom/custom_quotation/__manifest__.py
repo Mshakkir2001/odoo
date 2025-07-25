7192086e5ab9cc9483a7ef1a1c37fccedaf133f5
shakkir <shakkirtshakkirt86827@gmail.com>
2025-07-25 16:06:09 +0530

    update

---
+++ custom/custom_quotation/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Custom Quotation PDF',
+    'version': '1.0',
+    'category': 'Sales',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_report_inherit.xml',
+    ],
+    'installable': True,
+}
